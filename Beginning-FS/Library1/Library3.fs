a81ecc676f958a72ff3cdbbb6cf245410e1f577a
bernardo <epiphonetodie@gmail.com>
2019-01-07 16:54:39 +0100

    -

--- Beginning-FS/Library1/Library3.fs
+++ Beginning-FS/Library1/Library3.fs
@@ -35,3 +35,19 @@ let printMessages() =
     printfn "%s" message
 
 printMessages()
+
+// function that returns a function to
+let calculatePrefixFunction prefix =
+    // calculate prefix
+    let prefix' = Printf.sprintf "[%s]: " prefix
+    // define function to perform prefixing
+    let prefixFunction appendee =
+        Printf.sprintf "%s%s" prefix' appendee
+    // return function
+    prefixFunction
+
+// create the prefix function
+let prefixer = calculatePrefixFunction "DEBUG"
+
+// use the prefix function
+printfn "%s" (prefixer "My message")
